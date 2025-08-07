d2419a4db4fca576893024e7d9cc8ec3d02ffb5f
Pengfei <44221035+Pengfei196@users.noreply.github.com>
2025-08-07 15:07:47 +0800

    Add files via upload

---
+++ Tunnel_client.py
@@ -0,0 +1,40 @@
+import socket
+import threading
+
+def start_tcp_client(host='127.0.0.1', port=8889):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
+    print(f"[+] Connected to the server {host}:{port}")
+    return client_socket
+
+def forward(source, destination):
+    while True:
+        data = source.recv(2048)
+        if not data:
+            destination.shutdown(socket.SHUT_WR)
+            break
+        destination.sendall(data)
+
+def main(tunnel_client_socket, tunnel_host, tunnel_port, socks5_port):
+    # Respond to remote requests and create new tunnel sockets for transfer
+    while True:
+        try:
+            command = tunnel_client_socket.recv(100)
+            if command == b'create a new socket':
+                tunnel_client_socket.send(b'socket created')
+                new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                new_socket.connect(('127.0.0.1', socks5_port))
+                transfer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                transfer_socket.connect((tunnel_host, tunnel_port))
+                threading.Thread(target=forward, args=(transfer_socket, new_socket)).start()
+                threading.Thread(target=forward, args=(new_socket, transfer_socket)).start()
+        except KeyboardInterrupt:
+            print('Shutting down...')
+            break
+
+if __name__ == '__main__':
+    tunnel_host = '127.0.0.1'
+    tunnel_port = 8889
+    socks5_port = 9050
+    tunnel_client_socket = start_tcp_client(tunnel_host, tunnel_port)
+    main(tunnel_client_socket, tunnel_host, tunnel_port, socks5_port)
---
+++ Tunnel_server.py
@@ -0,0 +1,71 @@
+import socket
+import threading
+
+def forward(source, destination):
+    while True:
+        data = source.recv(2048)
+        if not data:
+            destination.shutdown(socket.SHUT_WR)
+            break
+        destination.sendall(data)
+
+class PseudoSocks5Server:
+    def __init__(self, local_port=9000, tunnel_host='0.0.0.0', tunnel_port=8889):
+        self.local_port = local_port
+        self.tunnel_host = tunnel_host
+        self.tunnel_port = tunnel_port
+        self.running = False
+
+    def start(self):
+        self.running = True
+
+        # waiting for a tunnel connection
+        tunnel_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tunnel_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Prevent port occupation
+        tunnel_sock.bind((self.tunnel_host, self.tunnel_port))
+        tunnel_sock.listen(5)
+        print(f"[*] Server startup, listening {self.tunnel_host}:{self.tunnel_port}")
+        tunnel_sock_client, client_addr = tunnel_sock.accept()
+        print(f"[+] Accept the connection from {client_addr}")
+        
+        try:
+            while self.running:
+                try:
+                    while True:
+                        # Listening a port for socks5 app
+                        server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                        server_sock.bind(('0.0.0.0', self.local_port))
+                        server_sock.listen(5)
+                        print(f'Socks5 client listening on port {self.local_port}')
+                        client_sock, addr = server_sock.accept()
+                        print(f'Accepted connection from {addr}')
+                        tunnel_sock_client.send(b'create a new socket')
+                        response = tunnel_sock_client.recv(100)
+                        if response == b'socket created':
+                            tunnel_transfer_sock_client, client_addr = tunnel_sock.accept()
+                        threading.Thread(target=forward, args=(client_sock, tunnel_transfer_sock_client)).start()
+                        threading.Thread(target=forward, args=(tunnel_transfer_sock_client, client_sock)).start()
+                except KeyboardInterrupt:
+                    print('Shutting down...')
+                    self.running = False
+                except Exception as e:
+                    print(f'Server error: {e}')
+        
+        finally:
+            server_sock.close()
+            tunnel_sock.close()
+            self.running = False
+
+
+if __name__ == '__main__':
+    # local_port: the pseudo socks5 server listening port which provide supply for application in local machine
+    # tunnel_host & tunnel_port: use to establish tunnel connection
+    socks5_server = PseudoSocks5Server(local_port=9000, tunnel_host='0.0.0.0', tunnel_port=8889)
+    socks5_server.start()
+
+
+# sudo proxychains apt download <package-name>
+# pip install --proxy socks5h://127.0.0.1:9000 numpy
+# curl -v  --proxy socks5h://127.0.0.1:9000 https://www.baidu.com
+# curl -v  --proxy socks5h://127.0.0.1:9000 http://www.google.com
